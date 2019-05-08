1bdf27cdd174962b7f1352d472b49f90048c4d9b
root <root@terraform>
2019-05-08 06:57:13 +0000

    Firt

--- 02-First_Deployment/first_deployment.tf
+++ 02-First_Deployment/first_deployment.tf
@@ -1,9 +1,9 @@
 provider "aws" {
   access_key = "Access Key"
   secret_key = "Secrey Key"
-  region     = "us-east-1"
+  region     = "us-west-2"
 }
 resource "aws_instance" "example" {
-  ami           = "ami-2757f631"
+  ami           = "ami-01e24be29428c15b2"
   instance_type = "t2.micro"
 }
